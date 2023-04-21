25dae029cb3fc8aeca49925e047ed879f4156003
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-04-21 20:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
     //Write your code here
+	let table = document.getElementById("myTable");
+	let rows = prompt("Input number of rows");
+	let cols = prompt("Input number of columns");
+	if(rows != null && cols != null){
+		for(let i = 0; i < rows; i++){
+			let row = document.createElement("tr");
+			for(let j = 0; j < cols; j++){
+				let cell = document.createElement("td");
+				cell.innerHTML = `Row-${i} Column-${j}`;
+				row.appendChild(cell);
+			}
+			table.appendChild(row);
+		}
+	}
+	
   
 }
